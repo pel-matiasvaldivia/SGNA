b986d77a34d2dff0115b3bbbea8a24cc02e22f5d
Matias <sysadmin@losandes.com.ar>
2026-05-18 12:31:02 -0300

    perf: Optimize all Dockerfiles with multi-stage builds, standalone next.js and dockerignores

---
+++ frontend/next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "standalone",
+  reactStrictMode: true,
+  // Disable telemetry during builds
+  telemetry: false,
+};
+
+export default nextConfig;
